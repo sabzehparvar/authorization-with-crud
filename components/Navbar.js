0d3d78f7842a2e57a89c0f11529b4181b49ccf4d
mojtaba <sabzehparvar.mojtaba@gmail.com>
2023-09-08 14:50:19 +0330

    navbar created

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useSelector } from "react-redux";
+
+function Navbar() {
+  const token = useSelector((state)=>state?.user?.auth?.token)
+  const email = useSelector((state)=> state?.user?.email)
+  const name = email?.split("@")[0]
+  
+  return (
+    <>
+      <nav className=" px-40 py-10 flex justify-between">
+        <ul className="flex items-center">
+          <li className="mr-8">
+            {
+              token ? (<h3>Hi {name}</h3>):''
+            }
+          </li>
+
+          <li className=" mr-8 ">
+            <Link href="dashboard">
+              <span>Dashboard</span>
+            </Link>
+          </li>
+
+          <li >
+            {token? (<Link href="/">Log out</Link>):(<Link href="/">Login</Link>)}
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
